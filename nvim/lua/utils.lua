99eb46db1e6250dca9837eae01d9d06a516e71ec
Robbert Ritsema <RobbertRitsema@protonmail.com>
2023-06-24 12:52:54 +0200

    zsh cleanup

--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -3,10 +3,8 @@
 -- neovim 0.7 has 'vim.pretty_print())
 function _G.dump(...)
   local objects = vim.tbl_map(vim.inspect, { ... })
-  print(unpack(objects))
+  print(table.unpack(objects))
 end
-
-local DEV_DIR = "$HOME/Sources/nvim"
 
 local M = {}
 
@@ -52,10 +50,6 @@ end
 
 function M.is_darwin()
   return vim.loop.os_uname().sysname == "Darwin"
-end
-
-function M.is_dev(path)
-  return vim.loop.fs_stat(string.format("%s/%s", vim.fn.expand(DEV_DIR), path))
 end
 
 function M.shell_error()
@@ -110,8 +104,8 @@ function M.get_visual_selection(nl_literal)
   local mode = vim.fn.mode()
   if mode == "v" or mode == "V" or mode == "" then
     -- if we are in visual mode use the live position
-    _, csrow, cscol, _ = unpack(vim.fn.getpos ".")
-    _, cerow, cecol, _ = unpack(vim.fn.getpos "v")
+    _, csrow, cscol, _ = table.unpack(vim.fn.getpos ".")
+    _, cerow, cecol, _ = table.unpack(vim.fn.getpos "v")
     if mode == "V" then
       -- visual line doesn't provide columns
       cscol, cecol = 0, 999
@@ -120,8 +114,8 @@ function M.get_visual_selection(nl_literal)
     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true)
   else
     -- otherwise, use the last known visual position
-    _, csrow, cscol, _ = unpack(vim.fn.getpos "'<")
-    _, cerow, cecol, _ = unpack(vim.fn.getpos "'>")
+    _, csrow, cscol, _ = table.unpack(vim.fn.getpos "'<")
+    _, cerow, cecol, _ = table.unpack(vim.fn.getpos "'>")
   end
   -- swap vars if needed
   if cerow < csrow then
