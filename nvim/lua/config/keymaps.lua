4993aef7c4da322f3b94fe1baba000c59c6ef46a
Robbert <RobbertRitsema@protonmail.com>
2024-07-22 22:03:46 +0200

    do keybind the proper way

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,36 +2,21 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 --
-local map = function(mode, lhs, rhs, opts)
-  local options = { noremap = true, silent = true }
-  if opts then
-    options = vim.tbl_extend("force", options, opts)
-  end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-end
-
-map("n", "<leader>w", "<cmd>w<cr><esc>")
-
--- Move Lines
-map("v", "K", ":m '<-2<cr>gv=gv", { desc = "Move Up" })
-map("v", "J", ":m '>+1<cr>gv=gv", { desc = "Move Down" })
+vim.keymap.del("n", "<leader>w")
+vim.keymap.set({ "i", "x", "n", "s" }, "<leader>w", "<cmd>w<cr><esc>", { desc = "Save File" })
 
 -- Don't put deleted text in default register
-map("n", "x", '"_x')
-map("v", "x", '"_x')
-map("n", "X", '"_X')
-map("v", "X", '"_X')
-map("n", "c", '"_c')
-map("n", "c", '"_c')
-map("v", "C", '"_C')
-map("v", "C", '"_C')
+vim.keymap.set({ "v", "n" }, "x", '"_X')
+vim.keymap.set({ "v", "n" }, "X", '"_X')
+vim.keymap.set({ "v", "n" }, "c", '"_c')
+vim.keymap.set({ "v", "n" }, "C", '"_C')
 
 -- Also, don't overwrite when pasting
-map("x", "p", '"_dP')
-map("x", "P", '"_dP')
+vim.keymap.set({ "x" }, "p", '"_dP')
+vim.keymap.set({ "x" }, "P", '"_dP')
 
 -- alt defaults
-map("n", "0", "^")
-map("n", "Y", "y$")
-map("n", "n", "nzzzv")
-map("n", "N", "Nzzzv")
+vim.keymap.set({ "n" }, "0", "^")
+vim.keymap.set({ "n" }, "Y", "y$")
+vim.keymap.set({ "n" }, "n", "nzzzv")
+vim.keymap.set({ "n" }, "N", "Nzzzv")
